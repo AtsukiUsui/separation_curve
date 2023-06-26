9a412c591eca3f0e5ae1a471c713fe6cd9a7eb14
AtsukiUsui <kmmm13037@gmail.com>
2023-06-26 15:02:18 +0900

    Add/2sigma_plot

--- single_bag_plot.py
+++ single_bag_plot.py
@@ -35,14 +35,20 @@ intensities = np_poses[:, 1]
 
 bin_ranges = np.arange(0, 100, 0.1)  # 100mmごとの範囲
 bin_intensities = []
+bin_intensities_2sigma = []  # 平均に2σを足した値を格納するリスト
+
 for bin_start in bin_ranges:
     bin_end = bin_start + 0.1
     mask = (ranges >= bin_start) & (ranges < bin_end)
     if np.any(mask):  # データが存在する場合のみ計算
         bin_intensity = np.mean(intensities[mask])
+        bin_std = np.std(intensities[mask])
+        bin_intensity_2sigma = bin_intensity + 2 * bin_std  # 平均に2σを足した値を計算
     else:
         bin_intensity = np.nan
+        bin_intensity_2sigma = np.nan
     bin_intensities.append(bin_intensity)
+    bin_intensities_2sigma.append(bin_intensity_2sigma)
 
 # プロット
 plt.subplot(111)
@@ -58,6 +64,9 @@ plt.scatter(np_poses[:, 0], np_poses[:, 1], s=4, c='g', alpha=0.3, label="renga"
 valid_indices = ~np.isnan(bin_intensities)
 plt.scatter(bin_ranges[valid_indices], np.array(bin_intensities)[valid_indices], c='r', marker='x', label="Average Intensity")
 
+# 平均+2σの値を青い点でプロット
+valid_indices_2sigma = ~np.isnan(bin_intensities_2sigma)
+plt.scatter(bin_ranges[valid_indices_2sigma], np.array(bin_intensities_2sigma)[valid_indices_2sigma], c='b', marker='o', label="Average + 2σ")
+
 plt.legend(fontsize=10)
 plt.show()
-
