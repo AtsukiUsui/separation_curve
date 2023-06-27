3c29faf5f70934015ec9596580734d49e36d9c8e
Atsuki Usui <kmmm13037@gmail.com>
2023-06-27 14:36:30 +0900

    Feature/ratio (#3)
    * Add/ratio
    
    * Add/print
    
    * Fix/(0,0)
    
    * Fix/minimal

--- double_bag_plot.py
+++ double_bag_plot.py
@@ -8,8 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # バッグファイルのパス
-bag_path_1 = os.path.join('bagfile', 'example_shiba.bag') # grass_bag
-bag_path_2 = os.path.join('bagfile', 'example_renga.bag') # renga_bag 
+bag_path_1 = os.path.join('bagfile', 'example_shiba.bag') # 芝生
+bag_path_2 = os.path.join('bagfile', 'example_renga.bag') #  
 bag_filename_1 = os.path.basename(bag_path_1)
 bag_filename_2 = os.path.basename(bag_path_2)
 
@@ -105,8 +105,9 @@ plt.subplot(111)
 plt.title(f"Range & Intensity ({bag_filename_1},{bag_filename_2}")
 plt.xlabel("Range [m]")
 plt.ylabel("Intensity")
-plt.xlim(2.5, 8)
-plt.ylim(0, 3500)
+plt.xlim(2, 7)
+# plt.ylim(0, 3500)
+
 
 # バッグファイル1のデータをプロット
 plt.scatter(np_poses_1[:, 0], np_poses_1[:, 1], s=4, c='c', alpha=0.3, label="grass")
@@ -134,19 +135,20 @@ plt.scatter(bin_ranges[valid_indices_2sigma_2], np.array(bin_intensities_2sigma_
 valid_indices_midpoint = valid_indices_2sigma_1 & valid_indices_2sigma_2
 plt.scatter(bin_ranges[valid_indices_midpoint], bin_intensities_midpoint[valid_indices_midpoint], c='orange', marker='o', label="Midpoint")
 
-# # 中間点の近似曲線を求める
+# 中間点の近似曲線を求める
 degree = 3  # 多項式の次数
 coefficients = np.polyfit(bin_ranges[valid_indices_midpoint], bin_intensities_midpoint[valid_indices_midpoint], degree)
 poly = np.poly1d(coefficients)  # 近似曲線の関数
 
 # 近似曲線の範囲を設定（近似曲線は元データに依存するので元データ大事）
-# curve_range = np.linspace(min(bin_ranges), max(bin_ranges), 100) # データがなくても近似曲線をプロットする
+# curve_range = np.linspace(np.min(bin_ranges), np.max(bin_ranges), 100) # データがなくても近似曲線をプロットする
 curve_range = np.linspace(bin_ranges[valid_indices_midpoint][0], bin_ranges[valid_indices_midpoint][-1], 100) #データがある部分までで止める
 
 # 近似曲線をプロット
 plt.plot(curve_range, poly(curve_range), 'k-', linewidth=4.0, label="Approximation Curve")
 
-# 近似曲線の式を表示
+
+# 近似曲線の式をグラフに表示
 equation_text = f"Approximation Curve: \n {poly}"
 plt.text(3, 200, equation_text, fontsize=12, color='black', bbox=dict(facecolor='w', edgecolor='red', boxstyle='square'))
 
@@ -154,14 +156,35 @@ plt.text(3, 200, equation_text, fontsize=12, color='black', bbox=dict(facecolor=
 print("Approximation Curve:")
 print(poly)
 
+# 芝生の認識率（分離曲線より大きい）
+intensity_vals_grass = np_poses_1[:, 1]
+valid_indices_grass = (np_poses_1[:, 0] != 0) & (intensity_vals_grass != 0)  # 距離と強度が0でないインデックスを取得
+ratio_grass = (np.sum(intensity_vals_grass[valid_indices_grass] > poly(np_poses_1[:, 0][valid_indices_grass])) / len(intensity_vals_grass[valid_indices_grass])) * 100
+ratio_grass = round(ratio_grass, 2)  # 小数点第2位までに制限
+
+print("芝生の点群数        :", np.sum(valid_indices_grass))
+print("分離曲線以下の点群数:", len(valid_indices_grass))
+print("->芝生の認識率      : %.2f%%\n" % ratio_grass)
+
+# レンガの認識率(分離曲線より小さい)
+intensity_vals_renga = np_poses_2[:, 1]
+valid_indices_renga = (np_poses_2[:, 0] != 0) & (intensity_vals_renga != 0)  # 距離と強度が0でないインデックスを取得
+ratio_renga = (np.sum(intensity_vals_renga[valid_indices_renga] < poly(np_poses_2[:, 0][valid_indices_renga])) / len(intensity_vals_renga[valid_indices_renga])) * 100
+ratio_renga = round(ratio_renga, 2)  # 小数点第2位までに制限
+
+print("レンガの点群数      :", np.sum(valid_indices_renga))
+print("分離曲線以下の点群数:", len(valid_indices_renga))
+print("->レンガの認識率    : %.2f%%" % ratio_renga)
+
+
 # 自動保存機能（保存名は、bagファイルの名前）
 # 保存パスの作成
 save_filename = f"Range & Intensity ({bag_filename_1}, {bag_filename_2}).png"
 save_path = os.path.join("plot", save_filename)
 
-plt.legend(fontsize=10)
+plt.legend(fontsize=10, loc='upper right')  # 凡例を右上に配置
 
 # グラフの保存
-plt.savefig(save_path, dpi=1000)
+plt.savefig(save_path, dpi=100)
 
 plt.show()
